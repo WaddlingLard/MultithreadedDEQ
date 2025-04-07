08ac39b66c47a46c8b4c959e30a55f94974aa450
brianwu <dathamor@gmail.com>
2025-04-07 02:09:52 -0600

    checking arg list input validation

--- hw4/main.c
+++ hw4/main.c
@@ -20,11 +20,11 @@
 static void *produce(void *a)
 {
   void **arg = a;
-  // if (!arg[0] || !arg[1])
-  // {
-  //   // Variables are not properly set when passing to produce
-  //   ERROR("Struct is not assembled properly");
-  // }
+  if (!arg[0] || !arg[1])
+  {
+    // Variables are not properly set when passing to produce
+    ERROR("Struct is not assembled properly");
+  }
   Deq q = (Deq)arg[0];
   Lawn l = (Lawn)arg[1];
   deq_tail_put(q, mole_new(l, 0, 0));
@@ -38,50 +38,56 @@ static void *consume(void *a)
   {
     ERROR("Invalid passing of deq");
   }
-  // void **arg = a;
-  // Deq q = (Deq)arg[0];
   Deq q = (Deq)a;
   mole_whack(deq_head_get(q));
   return 0;
 }
 
 // DOCUMENTATION
-void multiprocess(int n, pthread_t **producers, pthread_t **consumers,
-                  void *mp, void *deq)
+void multicreateproduce(int n, pthread_t **producers, void *mp)
 {
-
-  // MIGHT NEED TO REVISE
   for (int i = 0; i < n; i++)
   {
-    // Put the Mole at the front of the deq
-    // deq_head_put(jobs, produce(lawn));
-
     // MT variant to produce
     pthread_create(producers[i], 0, produce, mp);
-
-    // MT variant to consume
-    pthread_create(consumers[i], 0, consume, deq);
-
-    // Consume the Mole by pulling it off the deq though LIFO fashion
-    // consume(deq_tail_get(jobs));
   }
 }
 
-void multijoin(int n, pthread_t **producers, pthread_t **consumers)
+// DOCUMENTATION
+void multicreateconsume(int n, pthread_t **consumers, void *deq)
+{
+  for (int i = 0; i < n; i++)
+  {
+    // MT variant to consume
+    pthread_create(consumers[i], 0, consume, deq);
+  }
+}
+
+// DOCUMENTATION
+void multijoinproduce(int n, pthread_t **producers)
 {
   // Join the threads back together
   for (int i = 0; i < n; i++)
   {
     // Wait for produce thread to finish
     pthread_join(*producers[i], 0);
+  }
+}
 
-    // Wait for consumethread to finish
+// DOCUMENTATION
+void multijoinconsume(int n, pthread_t **consumers)
+{
+  // Join the consume threads back together
+  for (int i = 0; i < n; i++)
+  {
+    // Wait for consume thread to finish
     pthread_join(*consumers[i], 0);
   }
 }
 
 int main()
 {
+  // Used for the produce function
   struct Moleproduce
   {
     void *deq;
@@ -102,8 +108,11 @@ int main()
   mp.deq = lawn;
 
   // Execute multithreaded operations
-  multiprocess(n, (pthread_t **)producetids, (pthread_t **)consumetids, (void *)&mp, jobs);
-  multijoin(n, (pthread_t **)producetids, (pthread_t **)consumetids);
+  multicreateproduce(n, (pthread_t **)producetids, (void *)&mp);
+  multicreateconsume(n, (pthread_t **)consumetids, jobs);
+
+  multijoinproduce(n, (pthread_t **)producetids);
+  multijoinconsume(n, (pthread_t **)consumetids);
 
   lawn_free(lawn);
   deq_del(jobs, 0);
