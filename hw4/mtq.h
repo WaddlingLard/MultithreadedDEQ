a1a76b17db53c87026ffe4e36592539759f913b4
brianwu <dathamor@gmail.com>
2025-04-07 03:58:47 -0600

    redid declarations (added max size cap)

--- hw4/mtq.h
+++ hw4/mtq.h
@@ -5,7 +5,7 @@
 
 typedef void *Mtq;
 
-extern Mtq mtq_new();
+extern Mtq mtq_new(int max);
 extern void mtq_tail_put(Mtq mtq, Mole mole);
 extern Mole mtq_head_get(Mtq mtq);
 extern void mtq_del(Mtq mtq);
