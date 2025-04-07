af3734c124d59c83b20163a41470eb2865156a8c
brianwu <dathamor@gmail.com>
2025-04-07 02:42:44 -0600

    redid declarations

--- hw4/mtq.h
+++ hw4/mtq.h
@@ -1,11 +1,13 @@
 #ifndef MTQ_H
 #define MTQ_H
 
+#include "mole.h"
+
 typedef void *Mtq;
 
-extern Mtq newMtq();
-extern void putMtq();
-extern Data getMtq();
-extern void delMtq();
+extern Mtq mtq_new();
+extern void mtq_tail_put(Mtq mtq, Mole mole);
+extern Mole mtq_head_get(Mtq mtq);
+extern void mtq_del(Mtq mtq);
 
 #endif
