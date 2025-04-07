8e54945c772d1ce0f29f16e3b56301736b8fbd05
brianwu <dathamor@gmail.com>
2025-04-07 02:09:21 -0600

    added creation method

--- hw4/mtq.c
+++ hw4/mtq.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 
 #include "deq.h"
+#include "mtq.h"
+#include "error.h"
 
 typedef struct
 {
@@ -10,4 +12,46 @@ typedef struct
     // Will add condition variables later
     // pthread_cond_t condition;
     Deq *deq;
+    // Keep track of the multihreaded queue a different way
+    int size;
 } *Mtq;
+
+// Functions need to create
+// Init
+
+// DOCUMENTATION
+extern Mtq newMtq()
+{
+    // Create the multithreaded queue
+    Mtq rep = (Mtq)malloc(sizeof *rep);
+    if (!rep)
+    {
+        // Memory failure
+        ERROR("ERROR: Malloc failed!");
+    }
+
+    // Initialize the mutex lock
+    pthread_mutex_init(&rep->mutexlock, 0);
+
+    // Add deq and starting size
+    rep->deq = deq_new();
+    if (!rep->deq)
+    {
+        // Deq instantiation failure
+        ERROR("ERROR: Creating deq failed!");
+    }
+    rep->size = 0;
+
+    // Return the deq
+    return rep;
+}
+
+// Destroy
+// DOCUMENTATION
+extern void delMtq()
+{
+}
+
+// Get
+// Put
+// Print for debug?
